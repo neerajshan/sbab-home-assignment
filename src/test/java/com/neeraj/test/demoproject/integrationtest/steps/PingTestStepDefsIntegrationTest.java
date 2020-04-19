874445d1e78e7cfbbee4e4b1ec25c7bcc376dbb8
Neeraj Shandilya <neeraj.shandilya@netent.com>
2020-04-20 01:55:40 +0200

    added Ping Integration Test

---
+++ src/test/java/com/neeraj/test/demoproject/integrationtest/steps/PingTestStepDefsIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.neeraj.test.demoproject.integrationtest.steps;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class PingTestStepDefsIntegrationTest extends DemoProjectIntegrationTest {
+
+
+    @When("^the client calls /ping$")
+    public void the_client_calls_ping() {
+        String restEndpointUrl = "http://localhost:" + port + "/ping";
+        String mediaType = "text/plain";
+        executeGet(restEndpointUrl, mediaType);
+    }
+
+
+    @Then("^the client receives server response (.+)$")
+    public void the_client_receives_server_response_ok(String response) {
+        assertThat(latestResponse.getBody(), is(response));
+    }
+
+
+}
