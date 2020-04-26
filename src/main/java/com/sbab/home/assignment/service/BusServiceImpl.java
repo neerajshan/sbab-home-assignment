15ba375c635a4f15d883cacff2852ff97b131bcc
Neeraj Shandilya <neeraj.shandilya@netent.com>
2020-04-26 14:05:03 +0200

    Missed File in checkin

--- src/main/java/com/sbab/home/assignment/service/BusServiceImpl.java
+++ src/main/java/com/sbab/home/assignment/service/BusServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -55,6 +56,7 @@ public class BusServiceImpl implements BusService {
 
     // intended to be called by schedulers
     @Override
+    @Transactional
     public void refresh(List<Businformation> businformationList) {
         busInformationRepository.deleteAll();
         busInformationRepository.saveAll(businformationList);
